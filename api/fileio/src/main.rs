15aa2e4f8fef21df3dd99a8accf83b1d7cc3a61c
jrhosk <jrh4dy@gmail.com>
2025-01-17 22:27:37 -0500

    Trying to add logging.

--- api/fileio/src/main.rs
+++ api/fileio/src/main.rs
@@ -1,17 +1,16 @@
-mod serialize;
+mod enigma;
 
-use std::fs;
-use std::io::ErrorKind;
-use serde_json;
-use serde;
+//use log;
+//use serde_json;
+//use serde;
 use std::path;
 
 
 fn main() {
-    println!("Reading json file...");
+    println!(target: "runinfo", "Reading json file...");
     let path = path::Path::new("C:\\Users\\17579\\Development\\nerv\\api\\file.download.json");
 
-    let file_info = serialize::serialize::decode(path);
+    let file_info = enigma::json::decode(path);
 
-    file_info.print();
+    //file_info.print();
 }
